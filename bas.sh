bcc0b9ce2bc133b1e36ede071be2e8621eb425b6
Андрей Войтенко <viy.2005@gmail.com>
2021-10-26 21:50:54 +0300

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "start BAS"
 echo -n > /etc/apt/sources.list
-echo "deb http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a file*.conf
-echo "deb-src http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a file*.conf
-echo "" | tee -a file*.conf
-echo "deb http://ftp.debian.org/debian bullseye-updates main contrib non-free" | tee -a file*.conf
-echo "deb-src http://ftp.debian.org/debian bullseye-updates main contrib non-free" | tee -a file*.conf
-echo "" | tee -a file*.conf
-echo "deb http://security.debian.org/ bullseye/updates main contrib non-free" | tee -a file*.conf
-echo "deb-src http://security.debian.org/ bullseye/updates main contrib non-free" | tee -a file*.conf
-echo "deb http://ftp.debian.org/debian bullseye-backports main contrib non-free" | tee -a file*.conf
+echo "deb http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a /etc/apt/sources.list
+echo "deb-src http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a /etc/apt/sources.list
+echo "" | tee -a /etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian bullseye-updates main contrib non-free" | tee -a /etc/apt/sources.list
+echo "deb-src http://ftp.debian.org/debian bullseye-updates main contrib non-free" | tee -a /etc/apt/sources.list
+echo "" | tee -a /etc/apt/sources.list
+echo "deb http://security.debian.org/ bullseye/updates main contrib non-free" | tee -a /etc/apt/sources.list
+echo "deb-src http://security.debian.org/ bullseye/updates main contrib non-free" | tee -a /etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian bullseye-backports main contrib non-free" | tee -a /etc/apt/sources.list
 echo "****************** finish BAS *******************"
