f9de646268ea754c8cf5430d2904f7d8dbff0092
Андрей Войтенко <viy.2005@gmail.com>
2021-10-26 22:55:53 +0300

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "start BAS"
+echo "------------------ start BAS ----------------------"
+echo ""
 echo -n > /etc/apt/sources.list
 echo "deb http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a /etc/apt/sources.list
 ##echo "deb-src http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a /etc/apt/sources.list
@@ -10,7 +11,13 @@ echo "deb http://ftp.debian.org/debian bullseye main contrib non-free" | tee -a 
 ##echo "deb http://security.debian.org/ bullseye/updates main contrib non-free" | tee -a /etc/apt/sources.list
 ##echo "deb-src http://security.debian.org/ bullseye/updates main contrib non-free" | tee -a /etc/apt/sources.list
 ##echo "deb http://ftp.debian.org/debian bullseye-backports main contrib non-free" | tee -a /etc/apt/sources.list
+echo "-----------> apt update"
 apt update
+echo "-----------> apt install -y mc"
 apt install -y mc
+echo "-----------> set timezone"
+timedatectl set-timezone Europe/Kiev
+systemctl restart systemd-timesyncd.service
 #apt install ssh ntp mc -y
+echo ""
 echo "****************** finish BAS *******************"
